32de262d0f545b9f20e1469ca22159a6b5b4794d
Ewan Tindale <ewannnnn@gmail.com>
2020-08-18 17:00:31 +0100

    customize homepage

--- client/app/components/Home.tsx
+++ client/app/components/Home.tsx
@@ -6,8 +6,8 @@ import styles from './Home.css';
 export default function Home(): JSX.Element {
   return (
     <div className={styles.container} data-tid="container">
-      <h2>Home</h2>
-      <Link to={routes.COUNTER}>to Counter</Link>
+      <h2>chat app</h2>
+      {/* <Link to={routes.COUNTER}>to Counter</Link> */}
     </div>
   );
 }
