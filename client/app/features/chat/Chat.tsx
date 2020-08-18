9f39c9d187e4e86d6bcf53ddca9c0c3d68efcc53
Ewan Tindale <ewannnnn@gmail.com>
2020-08-18 22:06:41 +0100

    add logout button

--- client/app/features/chat/Chat.tsx
+++ client/app/features/chat/Chat.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import routes from '../../constants/routes.json';
+import styles from './Chat.css';
 
 export default function Chat() {
-  return <div>Chat</div>;
+  return (
+    <div className={styles.container}>
+      <h2>Chat</h2>
+
+      <Link to={routes.HOME}>Logout</Link>
+    </div>
+  );
 }
